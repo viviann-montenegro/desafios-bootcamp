e058cecbb2fe90a6484f17dbfe80575564f2048d
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2020-12-06 14:58:02 -0300

    desafio 2.1

---
+++ desafio2_1/HoraDaCorrida.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace desafio2_1
+{
+    class HoraDaCorrida
+    {
+        static void Main(string[] args)
+        {
+            string[] texto = Console.ReadLine().Split(" ");
+            int laps = int.Parse(texto[0]);
+            int signs = int.Parse(texto[1]);
+
+            int total = laps * signs;
+
+            for (int i = 10; i <= 90; i += 10)
+            {
+                double result = (double)total / 100 * i;
+                Console.Write(Math.Ceiling(result) + " ");
+            }
+        }
+    }
+}
