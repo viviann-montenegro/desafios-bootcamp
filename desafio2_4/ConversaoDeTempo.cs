7ec10b01a2ff5a76ba6a78e057c591eb64295327
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2020-12-06 16:23:11 -0300

    desafio 2.4

---
+++ desafio2_4/ConversaoDeTempo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace desafio2_4
+{
+    class ConversaoDeTempo
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            var horas = N/3600;
+            var minutos = (N%3600)/60;
+            var segundos = (N%3600)%60;
+
+            Console.Write($"{horas}:{minutos}:{segundos}");
+        }
+    }
+}
