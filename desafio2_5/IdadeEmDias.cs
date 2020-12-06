f853e9bcaa129a43b24a3d1e9d154914420813ed
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2020-12-06 16:31:49 -0300

    desafio 2.5

---
+++ desafio2_5/IdadeEmDias.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace desafio2_5
+{
+    class IdadeEmDias
+    {
+        static void Main(string[] args)
+        {
+            var idade = int.Parse(Console.ReadLine());
+
+            var anos = idade/365;
+            var meses = (idade%365)/30;
+            var dias = (idade%365)%30;
+
+            Console.WriteLine($"{anos} ano(s)");
+            Console.WriteLine($"{meses} mes(es)");
+            Console.WriteLine($"{dias} dia(s)");
+        }
+    }
+}
