44c9b381dddc4629c9041e57e97b556f411421dc
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2020-12-06 16:40:31 -0300

    desafio 2.6

---
+++ desafio2_6/TempoDoDobby.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace desafio2_6
+{
+    class TempoDoDobby
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            string[] line = Console.ReadLine().Split();
+            var A = int.Parse(line[0]);
+            var B = int.Parse(line[1]);
+
+            if(N >= (A+B))
+            {
+                Console.Write("Farei hoje!");
+            } 
+            else 
+            {
+                Console.Write("Deixa para amanha!");
+            }
+        }
+    }
+}
