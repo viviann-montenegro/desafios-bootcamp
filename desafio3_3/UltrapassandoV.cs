33522bc61e6f56a2c3ba61c0e2a3c23e8cb02d5b
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2020-12-12 16:49:36 -0300

    desafio 3.3

---
+++ desafio3_3/UltrapassandoV.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace desafio3_3
+{
+    class UltrapassandoV
+    {
+        static void Main(string[] args)
+        {
+            int R = Int32.Parse(Console.ReadLine());
+            int V = 0;
+            int soma = R;
+            int aux = R + 1;
+            int cont = 1;
+            
+            while(R >= V)
+            {
+              V = Int32.Parse(Console.ReadLine());
+            }
+            
+            while(soma <= V)
+            {
+              soma += aux;
+              aux += 1;
+              cont += 1;
+            }
+            
+            Console.Write(cont);
+        }
+    }
+}
