f77970464708a5618052511d594a9108d2080d6f
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2020-12-12 16:50:07 -0300

    desafio 3.5

---
+++ desafio3_5/PedroBentoMundoDeOz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace desafio3_5
+{
+    class PedroBentoMundoDeOz
+    {
+        static void Main(string[] args) 
+        {
+            var joiasTotais = new List<string>();
+            while(true)
+            {
+                string joia = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(joia))
+                {
+                    joiasTotais.Add(joia);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            var joiasUnicas = joiasTotais.Distinct().ToList();
+            Console.WriteLine(joiasUnicas.Count);
+        }
+    }
+}
